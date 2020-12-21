83dfa5c28d35be4f90e487f045a410f96efaf873
monirajan <a.monisha.1995@gmail.com>
2020-12-21 19:24:33 +0530

    Vet list with updated Controller

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/Controllers/VetContrller.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/Controllers/VetContrller.java
@@ -1,14 +1,23 @@
 package guru.springframework.sfgpetclinic.Controllers;
 
+import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetContrller {
 
+    private final VetService vetService;
+
+    public VetContrller(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"vets","vets/vetIndex"})
-    public String listVets()
+    public String listVets(Model model)
     {
+        model.addAttribute("vets",vetService.findAll());
         return "vets/vetIndex";
     }
 }
